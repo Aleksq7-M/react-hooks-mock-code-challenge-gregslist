fd386167b46258ba55269e42f6f0aca10e4ec917
aleksq7 <morrisalex2001@gmail.com>
2021-10-20 18:34:30 -0600

    commit tree 03ad80bf991257085a4136605cfd45ab20d393ee

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([]);
+
+  function getListingsFromDB(){
+    fetch('http://localhost:6001/listings')
+    .then(r => r.json())
+    .then(data => setListings(data))
+  }
+
+  useEffect(() => {
+    getListingsFromDB()
+  }, [])
+
+  function handleListingDelete(deletedId){
+    const updatedListings = listings.filter(listing => listing.id !== deletedId)
+    setListings(updatedListings);
+  }
+
+  function handleSearchSubmit(searchValue){
+    if (searchValue !== ''){
+      const updatedListings = listings.filter(listing => listing.description.includes(searchValue))
+      setListings(updatedListings)
+    }else{
+      getListingsFromDB()
+    }
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearchSubmit={handleSearchSubmit}/>
+      <ListingsContainer listings={listings} onListingDelete={handleListingDelete} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({onSearchSubmit}) {
+
+  //search state lives here
+  //listings ONLY UPDATE ON SUBMIT this way
+  const [search, setSearch] = useState('');
+
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    onSearchSubmit(search);
   }
 
   return (
@@ -12,8 +18,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
